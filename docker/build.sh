4e028dc08f8bec7d3c0ab2569836abd01fd3796f
Daniel Rodriguez <soynegativo@gmail.com>
2019-10-09 12:30:56 +0200

    Fix jenkinsfile, build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -6,12 +6,9 @@ appVersion=$(mvn -q -Dexec.executable=echo -Dexec.args='${project.version}' --no
 echo $appVersion
 cp target/ejemploViafirma.war docker
 
-tag_version="registry.viafirma.com/viafirma/platform/platform-example:${appVersion}"
-latest_version="registry.viafirma.com/viafirma/platform/platform-example:latest"
+tag_version="registry.viafirma.com/viafirma/platform/platform-example:${appVersion}-$1"
 
 cd docker
 docker build -t ${tag_version} .
-docker tag ${tag_version} ${latest_version}
 
-docker push ${latest_version}
 docker push ${tag_version}
