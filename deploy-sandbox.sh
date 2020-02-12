1afe7b813036a3f4d10d5248cf9a5389319d76e6
Daniel Rodriguez <soynegativo@gmail.com>
2020-02-12 14:52:30 +0100

    Fix deployment to cloudTest

---
+++ deploy-sandbox.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+version=$(mvn -q \
+    -Dexec.executable=echo \
+    -Dexec.args='${project.version}' \
+    --non-recursive \
+    exec:exec)
+echo $version
+export KUBECONFIG=$HOME/.kube/viafirma-test-config && kubectl config --kubeconfig=$HOME/.kube/viafirma-test-config set-context viavansi
+rm -rf tmpDeploy
+mkdir tmpDeploy
+cp deploy/kubernetes/ci/* tmpDeploy
+cd tmpDeploy
+sed -i -- 's/\$VERSION/'${version}-sandbox'/g' *
+kubectl delete -f .
+kubectl apply -f .
+cd ..
